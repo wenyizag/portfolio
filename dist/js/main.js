ca5c8176fea40a5da3eaaf98ad5218e84d55b7a6
Wenyi Zhang <wenyizag@outlook.com>
2018-07-27 18:04:12 -0400

    menu button & js & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItem = document.querySelectorAll('.nav-item');
+
+let menuShow = false;
+
+menuBtn.addEventListener("click", menuToggle);
+
+function menuToggle(){
+	if(!menuShow){
+		menuBtn.classList.add("close");
+		menu.classList.add("show");
+		menuBranding.classList.add("show");
+		menuNav.classList.add("show");
+		navItem.forEach((item) => {item.classList.add("show")});
+	}else{
+		menuBtn.classList.remove("close");
+		menu.classList.remove("show");
+		menuBranding.classList.remove("show");
+		menuNav.classList.remove("show");
+		navItem.forEach((item) => {item.classList.remove("show")});
+	}
+
+	menuShow = ! menuShow;
+}
